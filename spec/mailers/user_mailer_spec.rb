de63b2ed1a4497cb307f350a06e98f872c86a695
James Scott Jr <jscott@brotherhoodmutual.com>
2015-12-17 12:05:39 -0500

    Improving test coverage

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,23 @@
+
+RSpec.describe UserMailer do
+ let(:user) {FactoryGirl.create(:user)}
+  
+  describe "password_reset" do
+    let(:mail) { UserMailer.password_reset(user) }
+    before :each do
+      user.generate_token(:password_reset_token)
+      user.save
+    end
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password Reset")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to include(user.password_reset_token)
+    end
+  end
+
+end
