01ef1e0bbcecb041fd34d3fd54039bcf6363eafa
James Scott Jr <jscott@brotherhoodmutual.com>
2015-12-19 21:35:45 -0500

    Adjusting Bootstrap page structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,4 +40,16 @@ module ApplicationHelper
     @ct_password_service
   end
 
+  def nav_link(link_text, link_path, http_method=nil)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, class: class_name) do
+      if http_method
+        link_to(link_text, link_path, method: http_method)
+      else
+        link_to(link_text, link_path)
+      end
+    end
+  end
+  
 end # End module
