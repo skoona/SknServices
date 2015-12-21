32050817f5c6c44cd21d5aa193818fb1aef76ef9
James Scott Jr <jscott@skoona.net>
2015-12-20 21:59:50 -0500

    Resolve duplicate rspec headers, and re-implement simple_for for bootstrap

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,29 +27,17 @@ end
 #         set default_url_options[:host], or set :only_path to true'
 Rails.application.routes.default_url_options[:host] = 'www.example.com'
 
-# Tell it to use xpath rather than css selectors
-Capybara.configure do |config|
-  config.default_host = "http://www.example.com"
-  config.app_host   = 'http://www.example.com'
-  #config.server_port = 3000
-  #config.default_selector = :xpath
-end
-
-Capybara.register_driver :mechanize do |app|
-  Capybara::Mechanize::Driver.new(app, :browser => :firefox)
-end
-
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
 
   # Include support methods
   config.include AuthenticationHelpers
   config.include ApplicationHelper
-  config.include Utilities
-  config.include TestDataSerializers
 
   config.include Rails.application.routes.url_helpers
   config.include Rails.application.routes.mounted_helpers
@@ -61,54 +49,7 @@ RSpec.configure do |config|
   # ref: https://github.com/thoughtbot/factory_girl/blob/master/GETTING_STARTED.md
   config.include FactoryGirl::Syntax::Methods
 
-  config.infer_spec_type_from_file_location!
-
-  Kernel.srand config.seed
-
-  config.order = :random
-  config.color = true
-  config.tty = false
-
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
-
-  # TODO: Causes the Feature tag to fail in Capybara specs
-  # config.disable_monkey_patching!  # -- breaks rspec runtime
-
-  config.warnings = false
-
-  if config.files_to_run.one?
-    config.formatter = :documentation
-  else
-    config.formatter = :progress  #:html, :textmate, :documentation
-  end
-
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-    expectations.syntax = :expect
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true # prefer true
-  end
-
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
   config.after(:each) do
-    DatabaseCleaner.clean
     Warden.test_reset!
   end
 
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,14 @@
-Capybara.asset_host = 'http://localhost:3000'
+# Capybara.asset_host = 'http://localhost:3000'
+
+# Tell it to use xpath rather than css selectors
+Capybara.configure do |config|
+  config.default_host = "http://www.example.com"
+  config.app_host   = 'http://www.example.com'
+  config.asset_host = 'http://localhost:3000'
+  #config.server_port = 3000
+  #config.default_selector = :xpath
+end
+
+Capybara.register_driver :mechanize do |app|
+  Capybara::Mechanize::Driver.new(app, :browser => :firefox)
+end
