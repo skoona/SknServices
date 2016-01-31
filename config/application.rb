f90c98c90ac00fba2841e362d5811a5cd79f0b47
James Scott Jr <jscott@skoona.net>
2016-01-31 00:00:35 -0500

    Updating the ability to switch to/from jruby.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'java'
+if defined? JRUBY_VERSION
+  require 'java'
+end
 
 module SknService
   class Application < Rails::Application
