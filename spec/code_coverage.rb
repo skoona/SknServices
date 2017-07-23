bb3f57f3b8b6b83cc9be1d985c0aeefd2ed8aff3
James Scott <JScott@brotherhoodmutual.com>
2017-07-23 01:09:44 -0400

    Updated Documentation and Specs

--- spec/code_coverage.rb
+++ spec/code_coverage.rb
@@ -12,9 +12,7 @@ if "true".eql?ENV["COVERAGE"] and "test".eql?ENV["RAILS_ENV"]
     add_filter '/controlled/'
     add_filter '/db/'
     add_filter '/doc/'
-    add_filter '/lib/tasks/'
-    add_filter '/lib/assets/'
-    add_filter '/lib/templates/'
+    add_filter '/lib/'
     add_filter '/log/'
     add_filter '/META-INF/'
     add_filter '/public/'
@@ -22,20 +20,23 @@ if "true".eql?ENV["COVERAGE"] and "test".eql?ENV["RAILS_ENV"]
     add_filter '/vendor/'
 
     add_group 'Utility' do |src_file|
-        src_file.filename.include? 'lib/utility'
+        src_file.filename.include? 'app/strategy/utility'
     end
-    add_group 'Services Strategy' do |src_file|
-      ['app/strategy.domains','app/strategy.services',
-       'lib/registry','lib/providers',
-       'app/beans'].any? do |item|
+    add_group 'Strategy' do |src_file|
+      ['app/strategy/domains','app/strategy/services',
+       'app/strategy/registry','app/strategy/providers',
+       'app/strategy/processors', 'app/beans'].any? do |item|
         src_file.filename.include? item
       end
     end
-    add_group 'Security Services' do |src_file|
-      src_file.filename.include? 'lib/secure'
+    add_group 'Security' do |src_file|
+      src_file.filename.include? 'app/strategy/secure'
     end
     add_group "Controllers" do |src_file|
       src_file.filename.include?  "app/controllers"
+    end
+    add_group "Models" do |src_file|
+      src_file.filename.include?  "app/models"
     end
   end
 end
